3f660181e0e8085116d9454f7dc4d53f9a3db298
VNKirichenko <VNKirichenko@inno.tech>
2025-12-11 15:11:33 +0300

    Домашнее задание 1.base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let discriminant = b ** 2 - 4 * a * c
+
+	if (discriminant > 0)
+		return [
+			(-b + Math.sqrt(discriminant)) / (2 * a),
+			(-b - Math.sqrt(discriminant)) / (2 * a),
+		];
+	else if (discriminant === 0) return [-b / (2 * a)];
+
+	return [];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentMonth = percent / 100 / 12;
+	return +((amount - contribution) * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1))) * countMonths).toFixed(2);
 }
