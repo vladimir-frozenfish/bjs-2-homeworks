a01182d0a29aec81b9a531ae8b550d288a870844
VNKirichenko <VNKirichenko@inno.tech>
2025-12-12 16:51:17 +0300

    Домашнее задание 2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,47 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+	return {
+		min: Math.min(...arr),
+		max: Math.max(...arr),
+		avg: +(arr.reduce((total, current) => total + current, 0) / arr.length).toFixed(2),
+	};
 }
 
 function summElementsWorker(...arr) {
-
+	return arr.reduce((total, current) => total + current, 0)
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	return arr.length ? (Math.max(...arr) - Math.min(...arr)) : 0;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+	let sumOfOdd = 0;
+	let sumOfEven = 0;
+	for (let num of arr) {
+		num % 2 ? (sumOfOdd += num) : (sumOfEven += num);
+	}
+	return sumOfEven - sumOfOdd;
 }
 
 function averageEvenElementsWorker(...arr) {
+	if (!arr.length) return 0
 
+	let countOfEven = 0;
+	let sumOfEven = 0;
+	for (let num of arr) {
+		if (!(num % 2)) {
+			sumOfEven += num;
+			countOfEven++;
+		}
+	}
+	return sumOfEven / countOfEven;
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+	let result = []
+	for (let arr of arrOfArr) {
+		result.push(func(...arr));
+	}
 
+	return Math.max(...result);
 }
