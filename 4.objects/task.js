c0fb2f571a52c7cd9085333878c6f750383844e1
VNKirichenko <VNKirichenko@inno.tech>
2025-12-18 10:11:44 +0300

    Домашнее задание 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+	this.name = name
+	this.gender = gender
+	this.age = age
+	this.marks = []
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks) {
+		this.marks = [...this.marks, ...marks]
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) return 0
+	return this.marks.reduce((total, current) => {
+		return total + current
+	}, 0) / this.marks.length
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject
+	delete this.marks
+	this.excluded = reason
 }
