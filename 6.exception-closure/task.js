a22a16a8bb21928b4b8975a8eeb20b6b02686c41
VNKirichenko <VNKirichenko@inno.tech>
2025-12-26 09:35:18 +0300

    Домашнее задание 6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿const parseCount = (number) => {
+	let parseNumber = Number.parseFloat(number)
+
+	if (!Number.isNaN(parseNumber)) return parseNumber
+	else throw new Error('Невалидное значение')
+}
+
+const validateCount = (number) => {
+	try {
+		return parseCount(number)
+	} catch (e) {
+		return e
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if (
+			a + b < c || a + c < b || b + c < a
+		) {
+			throw new Error('Треугольник с такими сторонами не существует')
+		}
+
+		this.a = a
+		this.b = b
+		this.c = c
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c
+	}
+
+	get area() {
+		let halfPerimeter = this.perimeter / 2
+		return +Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c)).toFixed(3)
+	}
+}
+
+const getTriangle = (a, b, c) => {
+	try {
+		return new Triangle(a, b, c)
+	} catch {
+		return {
+			get area() {
+				return 'Ошибка! Треугольник не существует'
+			},
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует'
+			}
+		}
+	}
+}
