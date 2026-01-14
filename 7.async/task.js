d51a03c670387f0c7e1618ee5bc3b641cb2f6806
VNKirichenko <VNKirichenko@inno.tech>
2026-01-14 14:55:53 +0300

    Домашнее задание 7.async

---
+++ 7.async/task.js
@@ -0,0 +1,48 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = []
+    this.intervalId = null
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) throw new Error('Отсутствуют обязательные аргументы')
+    if (this.alarmCollection.find(item => item.time === time)) console.warn('Уже присутствует звонок на это же время')
+    this.alarmCollection.push({callback, time, canCall: true})
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(item => item.time !== time)
+  }
+
+  getCurrentFormattedTime() {
+    let currentDate = new Date()
+    return `${currentDate.getHours()}:${currentDate.getMinutes()}`
+  }
+
+  start() {
+    if (this.intervalId) return
+
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach(item => {
+        if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+          item.canCall = false
+          item.callback()
+        }
+      })
+    }, 1000)
+  }
+
+  stop() {
+    clearInterval(this.intervalId)
+    this.intervalId = null
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(item => item.canCall = true)
+  }
+
+  clearAlarms() {
+    this.stop()
+    this.alarmCollection = []
+  }
+}
